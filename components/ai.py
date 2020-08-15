82224cf1ec86899af1c3cb7499a7c1aab39d475e
drewww <drew.harry@gmail.com>
2020-08-15 00:03:38 -0700

    Staged movement working. Currently all enemies rush me like whoa.

--- components/ai.py
+++ components/ai.py
@@ -70,38 +70,51 @@ class HostileEnemy(BaseAI):
         # for now, what needs to happen is we need to know if there's a current
         # uninterrupted sightline between the hostile and the player.
         # if yes, check if we're facing. if we are, fire. otherwise, rotate.
-        if self.engine.game_map.visible[self.entity.x, self.entity.y]:
+        # if self.engine.game_map.visible[self.entity.x, self.entity.y]:
 
-            visibility = tcod.map.compute_fov(
-                self.engine.game_map.tiles["transparent"], (self.entity.x, self.entity.y))
+        visibility = tcod.map.compute_fov(
+            self.engine.game_map.tiles["transparent"], (self.entity.x, self.entity.y))
 
-            target_is_visible = visibility[target.x, target.y]
-            print(f'{self} -> ({target.x}, {target.y}) = {target_is_visible}')
+        target_is_visible = visibility[target.x, target.y]
+        print(f'{self} -> ({target.x}, {target.y}) = {target_is_visible}')
 
-            if target_is_visible:
-                if distance <= 1:
-                    print(f'too close to attack!')
-                    # return MeleeAction(self.entity, dx, dy).perform()
-                if distance > 1:
-                    # oh actually, this is easy! get angle,
-                    # check if it collapses to facing. done.
-                    firing_direction = Facing.get_direction(self.entity.x, self.entity.y, target.x, target.y)
-                    print(f'firing_facing: {firing_direction} + my_facing: {self.entity.facing}')
+        if target_is_visible:
+            if distance <= 1:
+                print(f'too close to attack!')
+                # return MeleeAction(self.entity, dx, dy).perform()
+            elif distance > 1 and distance <= 3:
+                # oh actually, this is easy! get angle,
+                # check if it collapses to facing. done.
+                firing_direction = Facing.get_direction(self.entity.x, self.entity.y, target.x, target.y)
+                print(f'firing_facing: {firing_direction} + my_facing: {self.entity.facing}')
 
-                    if(firing_direction == self.entity.facing):
-                        return ShootAction(self.entity, target.x-self.entity.x,
-                            target.y - self.entity.y).perform()
-                    else:
-                        # lets just assume we fully rotate to target immediately.
-                        print('not facing target, prepare to rotate')
-                        return RotateAction(self.entity, firing_direction).perform()
+                if(firing_direction == self.entity.facing):
+                    return ShootAction(self.entity, target.x-self.entity.x,
+                        target.y - self.entity.y).perform()
+                else:
+                    # lets just assume we fully rotate to target immediately.
+                    print('not facing target, prepare to rotate')
+                    return RotateAction(self.entity, firing_direction).perform()
             else:
+                # if they are visible but not in range, move to them.
                 self.path = self.get_path_to(target.x, target.y)
+        else:
+            # TODO this is obviously wrong now because enemies will ALWAYS heat seak to the player. but okay for now to test turn + move logic.
+            self.path = self.get_path_to(target.x, target.y)
 
         if self.path:
             dest_x, dest_y = self.path.pop(0)
-            return MovementAction(
-                self.entity, dest_x - self.entity.x, dest_y - self.entity.y,
-            ).perform()
+
+            moving_direction = Facing.get_direction(self.entity.x,
+                self.entity.y, dest_x, dest_y)
+
+            if(moving_direction == self.entity.facing):
+                return MovementAction(
+                    self.entity, dest_x - self.entity.x, dest_y - self.entity.y,
+                ).perform()
+            else:
+                return RotateAction(self.entity, moving_direction).perform()
+
+
 
         return WaitAction(self.entity).perform()
