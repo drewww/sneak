67ce51753600af46e5e642e00f6c9a8482af3bb7
drewww <drew.harry@gmail.com>
2020-08-14 22:55:00 -0700

    Visibility checking and range finding for enemies is working.

--- components/ai.py
+++ components/ai.py
@@ -53,17 +53,37 @@ class HostileEnemy(BaseAI):
         super().__init__(entity)
         self.path: List[Tuple[int, int]] = []
 
+    def __str__(self):
+        return f'HostileEnemy({self.entity.x}, {self.entity.y})'
+
     def perform(self) -> None:
         target = self.engine.player
         dx = target.x - self.entity.x
         dy = target.y - self.entity.y
         distance = max(abs(dx), abs(dy))  # Chebyshev distance.
 
-        if self.engine.game_map.visible[self.entity.x, self.entity.y]:
-            if distance <= 1:
-                return MeleeAction(self.entity, dx, dy).perform()
+        # wow this is a cheap trick. If the human can see me, then I act.
+        # this is where the logic around ambient pathing goes, TBD.
 
-            self.path = self.get_path_to(target.x, target.y)
+        # for now, what needs to happen is we need to know if there's a current
+        # uninterrupted sightline between the hostile and the player.
+        # if yes, check if we're facing. if we are, fire. otherwise, rotate.
+        if self.engine.game_map.visible[self.entity.x, self.entity.y]:
+
+            visibility = tcod.map.compute_fov(
+                self.engine.game_map.tiles["transparent"], (self.entity.x, self.entity.y))
+
+            target_is_visible = visibility[target.x, target.y]
+            print(f'{self} -> ({target.x}, {target.y}) = {target_is_visible}')
+
+            if target_is_visible:
+                if distance <= 1:
+                    print(f'too close to attack!')
+                    # return MeleeAction(self.entity, dx, dy).perform()
+                if distance > 1:
+                    print('enter ranged attack here')
+            else:
+                self.path = self.get_path_to(target.x, target.y)
 
         if self.path:
             dest_x, dest_y = self.path.pop(0)
