396daeb7693206817037d19bc990655890f48cda
drewww <drew.harry@gmail.com>
2020-08-18 21:47:03 -0700

    Created AI modes.

--- components/ai.py
+++ components/ai.py
@@ -9,11 +9,17 @@ from actions import Action, MeleeAction, MovementAction, WaitAction, RotateActio
 from components.base_component import BaseComponent
 
 from entity import Facing
+from enum import Enum, auto
+
 import color
 
 if TYPE_CHECKING:
     from entity import Actor
 
+class HostileMode(Enum):
+    PATROL = auto()
+    HUNT = auto()
+    REST = auto()
 
 class BaseAI(Action, BaseComponent):
     entity: Actor
@@ -56,75 +62,36 @@ class HostileEnemy(BaseAI):
         super().__init__(entity)
         self.path: List[Tuple[int, int]] = []
 
+        self.mode = HostileMode.PATROL
+        self.waypoint = None
+
     def __str__(self):
         return f'HostileEnemy({self.entity.x}, {self.entity.y})'
 
-    def perform(self) -> None:
-        target = self.engine.player
-        dx = target.x - self.entity.x
-        dy = target.y - self.entity.y
-        distance = max(abs(dx), abs(dy))  # Chebyshev distance.
-
-        # wow this is a cheap trick. If the human can see me, then I act.
-        # this is where the logic around ambient pathing goes, TBD.
-
-        # for now, what needs to happen is we need to know if there's a current
-        # uninterrupted sightline between the hostile and the player.
-        # if yes, check if we're facing. if we are, fire. otherwise, rotate.
-        # if self.engine.game_map.visible[self.entity.x, self.entity.y]:
-
-
-        # visibility = tcod.map.compute_fov(
-        #     self.engine.game_map.tiles["transparent"], (self.entity.x, self.entity.y))
-
+    def is_visible(self, x, y) -> Boolean:
         visibility = self.entity.get_visibility(
             self.engine.game_map.tiles["transparent"])
 
-        target_is_visible = visibility[target.x, target.y]
+        return visibility[x, y]
 
-        # reasons to drop a lock:
-        #   moving
-        #   target is out of sight
-        #   target is out of range
+    def perform(self) -> None:
 
-        if target_is_visible:
-            firing_direction = Facing.get_direction(self.entity.x,
-                self.entity.y, target.x, target.y)
+        if self.mode==HostileMode.PATROL:
+            # check if we happen to spot the player
+            player_visible = self.is_visible(self.engine.player.x,
+                self.engine.player.y)
 
-            if distance <= 1:
-                self.entity.target_lock = None
-                self.engine.message_log.add_message(
-                    f"You got in close, enemy lost their lock!", color.white
-                )
-            elif distance > 1 and distance <= 5:
-                # target lock now implies facing. if you move across a facing
-                # boundary it doesn't unlock.
-                if self.entity.target_lock==target:
-                    return ShootAction(self.entity, target.x-self.entity.x,
-                        target.y - self.entity.y).perform()
-                else:
-                    # lets just assume we fully rotate to target immediately.
-                    self.engine.message_log.add_message(
-                        f"You've been locked on to!.", color.enemy_atk
-                    )
-
-                    return TargetLockAction(self.entity, firing_direction, target).perform()
-
+            if player_visible:
+                self.mode=HostileMode.HUNT
             else:
-                # if they are visible but not in range, move to them.
-                # lock is held in this case because they can see you they just know they can't hit you yet.
-                self.path = self.get_path_to(target.x, target.y)
+                self.waypoint = (self.engine.player.x, self.engine.player.y)
+                self.path = self.get_path_to(self.waypoint[0], self.waypoint[1])
+                # if self.waypoint == None:
+                    # we need to set a new waypoint.
+        elif self.mode==HostileMode.HUNT:
+            return self.perform_hunt()
         else:
-            # TODO this is obviously wrong now because enemies will ALWAYS heat seak to the player. but okay for now to test turn + move logic.
-            self.path = self.get_path_to(target.x, target.y)
-
-            # also, if target not visible, drop the lock.
-            if self.entity.target_lock != None:
-                self.engine.message_log.add_message(
-                    f"The enemy lost sight of you.", color.white
-                )
-
-            self.entity.target_lock = None
+            print('Unhandled mode.')
 
         if self.path:
             dest_x, dest_y = self.path.pop(0)
@@ -140,3 +107,78 @@ class HostileEnemy(BaseAI):
                 return RotateAction(self.entity, moving_direction).perform()
 
         return WaitAction(self.entity).perform()
+
+    def perform_hunt(self):
+                target = self.engine.player
+
+                dx = target.x - self.entity.x
+                dy = target.y - self.entity.y
+                distance = max(abs(dx), abs(dy))  # Chebyshev distance.
+
+                # wow this is a cheap trick. If the human can see me, then I act.
+                # this is where the logic around ambient pathing goes, TBD.
+
+                # for now, what needs to happen is we need to know if there's a current
+                # uninterrupted sightline between the hostile and the player.
+                # if yes, check if we're facing. if we are, fire. otherwise, rotate.
+                # if self.engine.game_map.visible[self.entity.x, self.entity.y]:
+
+                target_is_visible = self.is_visible(target.x, target.y)
+
+                # reasons to drop a lock:
+                #   moving
+                #   target is out of sight
+                #   target is out of range
+
+                if target_is_visible:
+                    firing_direction = Facing.get_direction(self.entity.x,
+                        self.entity.y, target.x, target.y)
+
+                    if distance <= 1:
+                        self.entity.target_lock = None
+                        self.engine.message_log.add_message(
+                            f"You got in close, enemy lost their lock!", color.white
+                        )
+                    elif distance > 1 and distance <= 5:
+                        # target lock now implies facing. if you move across a facing
+                        # boundary it doesn't unlock.
+                        if self.entity.target_lock==target:
+                            return ShootAction(self.entity, target.x-self.entity.x,
+                                target.y - self.entity.y).perform()
+                        else:
+                            # lets just assume we fully rotate to target immediately.
+                            self.engine.message_log.add_message(
+                                f"You've been locked on to!.", color.enemy_atk
+                            )
+
+                            return TargetLockAction(self.entity, firing_direction, target).perform()
+
+                    else:
+                        # if they are visible but not in range, move to them.
+                        # lock is held in this case because they can see you they just know they can't hit you yet.
+
+                        # 8.18.20 -- actually, this should probably be change into SEARCH mode when that exists. Last known location. and also moving out of range but still being visible is a case we'll grapple with more when there's shotgun enemies. but that's a later problem.
+                        self.mode = HostileMode.PATROL
+                        self.entity.target_lock = None
+
+                        return WaitAction(self.entity).perform()
+                        self.path = self.get_path_to(target.x, target.y)
+                else:
+
+                    # if target is not visibile, return to patrol.
+                    # eventually this should be a SEARCH variant.
+                    self.mode = HostileMode.PATROL
+                    self.entity.target_lock = None
+
+                    return WaitAction(self.entity).perform()
+
+                    # TODO this is obviously wrong now because enemies will ALWAYS heat seak to the player. but okay for now to test turn + move logic.
+                    # self.path = self.get_path_to(target.x, target.y)
+                    #
+                    # # also, if target not visible, drop the lock.
+                    # if self.entity.target_lock != None:
+                    #     self.engine.message_log.add_message(
+                    #         f"The enemy lost sight of you.", color.white
+                    #     )
+                    #
+                    # self.entity.target_lock = None
